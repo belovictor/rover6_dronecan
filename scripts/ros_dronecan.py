485a1008139821e856be57509becefe6ef486559
Victor Belov <belovictor@gmail.com>
2023-07-01 21:32:29 +0000

    Fix battery message

--- scripts/ros_dronecan.py
+++ scripts/ros_dronecan.py
@@ -9,7 +9,6 @@ class RosDronecan():
         self.can_interface = can_interface
         self.node_id = node_id
         self.bitrate = bitrate
-        rospy.init_node('roscyphal')
         rospy.loginfo("Setting Up the dronecan bridge...")
     
     def run(self):
@@ -24,7 +23,7 @@ class RosDronecan():
         self.node.add_handler(dronecan.uavcan.equipment.power.BatteryInfo, self.node_battery_status_callback)
         while not rospy.is_shutdown():
             try:
-                self.node.spin(0)
+                self.node.spin(1)
             except dronecan.transport.TransferError:
                 rospy.logerr("dronecan transfer error")
         self.node.close()
@@ -36,11 +35,13 @@ class RosDronecan():
         battery_status.voltage = event.message.voltage
         battery_status.current = event.message.current
         battery_status.percentage = event.message.state_of_charge_pct
+        battery_status.temperature = event.message.temperature - 273.15
         self._ros_pub_battery_state.publish(battery_status)
 
 if __name__ == "__main__":
-    bridge = RosDronecan(can_interface=rospy.get_param('can_interface', "can0"),
-                         node_id=rospy.get_param('node_id', 127),
-                         bitrate=rospy.get_param('bitrate', 1000000))
+    rospy.init_node('rosdronecan')
+    bridge = RosDronecan(can_interface=rospy.get_param('~can_interface', "can0"),
+                         node_id=rospy.get_param('~node_id', 127),
+                         bitrate=rospy.get_param('~bitrate', 1000000))
     bridge.run()
 
