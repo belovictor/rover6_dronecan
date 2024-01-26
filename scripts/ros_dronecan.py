995aba332b6863b9dc1c883720efc3f155d3af78
Victor Belov <belovictor@gmail.com>
2024-01-26 23:31:21 +0300

    Extend BatteryInfo functionality to support monitors reporting only current and voltage

--- scripts/ros_dronecan.py
+++ scripts/ros_dronecan.py
@@ -5,11 +5,19 @@ import dronecan
 from sensor_msgs.msg import BatteryState
 
 class RosDronecan():
-    def __init__(self, can_interface="can0", node_id=127, bitrate=1000000):
-        self.can_interface = can_interface
-        self.node_id = node_id
-        self.bitrate = bitrate
+    def __init__(self):
         rospy.loginfo("Setting Up the dronecan bridge...")
+        self.can_interface = rospy.get_param('~can_interface', 'can0')
+        self.node_id = rospy.get_param('~node_id', 127)
+        self.bitrate = rospy.get_param('~bitrate', 1000000)
+        self.current_offset = rospy.get_param('~current_offset', 0.0)
+        self.calculate_percentage = rospy.get_param('~calculate_percentage', False)
+        self.cell_empty = rospy.get_param('~cell_empty', 3.3)
+        self.cell_full = rospy.get_param('~cell_full', 4.2)
+        self.cell_num = rospy.get_param('~cell_num', 6)
+        self.negative_charge = rospy.get_param('~calculate_percentage', True)
+        self.battery_empty = self.cell_empty * self.cell_num
+        self.battery_full = self.cell_full * self.cell_num
     
     def run(self):
         rospy.loginfo("Starting dronecan bridge on interface %s", self.can_interface)
@@ -34,16 +42,25 @@ class RosDronecan():
         # rospy.loginfo(dronecan.to_yaml(event))
         battery_status = BatteryState()
         battery_status.voltage = event.message.voltage
-        battery_status.current = event.message.current
-        battery_status.percentage = event.message.state_of_charge_pct
+        battery_status.current = event.message.current + self.current_offset
         battery_status.temperature = event.message.temperature - 273.15
-        battery_status.power_supply_status = 2
+        if battery_status.current > 0:
+            if self.negative_charge:
+                battery_status.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_DISCHARGING
+            else:
+                battery_status.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_CHARGING
+        else:
+            if self.negative_charge:
+                battery_status.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_CHARGING
+            else:
+                battery_status.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_DISCHARGING
+        if self.calculate_percentage:
+            battery_status.percentage = (event.message.voltage - self.battery_empty) / (self.battery_full - self.battery_empty) * 100
+        else:
+            battery_status.percentage = event.message.state_of_charge_pct
         self._ros_pub_battery_state.publish(battery_status)
 
 if __name__ == "__main__":
     rospy.init_node('rosdronecan')
-    bridge = RosDronecan(can_interface=rospy.get_param('~can_interface', "can0"),
-                         node_id=rospy.get_param('~node_id', 127),
-                         bitrate=rospy.get_param('~bitrate', 1000000))
+    bridge = RosDronecan()
     bridge.run()
-
